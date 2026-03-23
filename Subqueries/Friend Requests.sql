e2188323adc32e728d8d2c19aa60b5bed2fff95c
Adeoluwa Babatope <145295882+AdeMoni@users.noreply.github.com>
2026-03-22 20:42:05 -0500

    Add files via upload

---
+++ Subqueries/Friend Requests.sql
@@ -0,0 +1,44 @@
+/*
+Table: RequestAccepted
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| requester_id   | int     |
+| accepter_id    | int     |
+| accept_date    | date    |
++----------------+---------+
+
+(requester_id, accepter_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the ID of the user who sent the request, the ID of the user who received the request, and the date when the request was accepted.
+
+Write a solution to find the people who have the most friends and the most friends number.
+
+The test cases are generated so that only one person has the most friends.
+
+The result format is in the following example.
+
+Follow up: In the real world, multiple people could have the same most number of friends. Could you find all these people in this case?
+
+*/
+
+WITH FriendCounts AS (
+    SELECT 
+        id, 
+        COUNT(*) AS num
+    FROM (
+        SELECT requester_id AS id FROM RequestAccepted
+        UNION ALL
+        SELECT accepter_id AS id FROM RequestAccepted
+    ) AS all_ids
+    GROUP BY id
+),
+RankedFriends AS (
+    SELECT 
+        id, 
+        num,
+        RANK() OVER (ORDER BY num DESC) AS rnk
+    FROM FriendCounts
+)
+SELECT id, num
+FROM RankedFriends
+WHERE rnk = 1;
